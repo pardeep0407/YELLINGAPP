efcd28bdb05d1015b15195979be67f76aaecd4de
pardeep0407 <39106963+pardeep0407@users.noreply.github.com>
2019-05-25 12:00:28 -0400

    R2--RED(NO NEED FOR REFACTOR R1)

---
+++ Yelling.java
@@ -0,0 +1,12 @@
+
+public class Yelling 
+{
+ public String Scream (String name)
+ {
+	 if(name == "Peter")
+	  {
+	 return "Peter is yelling";
+	 }
+	return name; 
+ }
+}
---
+++ YellingTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+public class YellingTest {
+@Before
+public void setUp() throws Exception {
+}
+@After
+public void tearDown() throws Exception {
+}
+@Test
+public void test1PersonIsYelling() {
+// 1 PERSON IS YELLINg
+Yelling y = new Yelling();
+String result = y.Scream("Peter");
+assertEquals("Peter is yelling",result );
+}
+@Test
+public void test0PersonIsYelling() {
+// 0  IS YELLINg
+Yelling y = new Yelling();
+String result = y.Scream("");
+assertEquals("Nobody is yelling",result );
+}
+}
